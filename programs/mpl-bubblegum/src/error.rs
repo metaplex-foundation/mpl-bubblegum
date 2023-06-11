6b1b8d83816fb2b2d73364365a3671a7b4921838
Loris Leiva <loris.leiva@gmail.com>
2023-06-11 12:56:50 +0100

    Move program and solita folders

--- programs/mpl-bubblegum/src/error.rs
+++ programs/mpl-bubblegum/src/error.rs
@@ -1,38 +1,59 @@
-use num_derive::FromPrimitive;
-use solana_program::{
-    decode_error::DecodeError,
-    msg,
-    program_error::{PrintProgramError, ProgramError},
-};
-use thiserror::Error;
+use anchor_lang::prelude::*;
 
-#[derive(Error, Clone, Debug, Eq, PartialEq, FromPrimitive)]
-pub enum MplBubblegumError {
-    /// 0 - Invalid System Program
-    #[error("Invalid System Program")]
-    InvalidSystemProgram,
-    /// 1 - Error deserializing account
-    #[error("Error deserializing account")]
-    DeserializationError,
-    /// 2 - Error serializing account
-    #[error("Error serializing account")]
-    SerializationError,
-}
-
-impl PrintProgramError for MplBubblegumError {
-    fn print<E>(&self) {
-        msg!(&self.to_string());
-    }
-}
-
-impl From<MplBubblegumError> for ProgramError {
-    fn from(e: MplBubblegumError) -> Self {
-        ProgramError::Custom(e as u32)
-    }
-}
-
-impl<T> DecodeError<T> for MplBubblegumError {
-    fn type_of() -> &'static str {
-        "Mpl Bubblegum Error"
-    }
+#[error_code]
+pub enum BubblegumError {
+    #[msg("Asset Owner Does not match")]
+    AssetOwnerMismatch,
+    #[msg("PublicKeyMismatch")]
+    PublicKeyMismatch,
+    #[msg("Hashing Mismatch Within Leaf Schema")]
+    HashingMismatch,
+    #[msg("Unsupported Schema Version")]
+    UnsupportedSchemaVersion,
+    #[msg("Creator shares must sum to 100")]
+    CreatorShareTotalMustBe100,
+    #[msg("No duplicate creator addresses in metadata")]
+    DuplicateCreatorAddress,
+    #[msg("Creator did not verify the metadata")]
+    CreatorDidNotVerify,
+    #[msg("Creator not found in creator Vec")]
+    CreatorNotFound,
+    #[msg("No creators in creator Vec")]
+    NoCreatorsPresent,
+    #[msg("User-provided creator Vec must result in same user-provided creator hash")]
+    CreatorHashMismatch,
+    #[msg("User-provided metadata must result in same user-provided data hash")]
+    DataHashMismatch,
+    #[msg("Creators list too long")]
+    CreatorsTooLong,
+    #[msg("Name in metadata is too long")]
+    MetadataNameTooLong,
+    #[msg("Symbol in metadata is too long")]
+    MetadataSymbolTooLong,
+    #[msg("Uri in metadata is too long")]
+    MetadataUriTooLong,
+    #[msg("Basis points in metadata cannot exceed 10000")]
+    MetadataBasisPointsTooHigh,
+    #[msg("Tree creator or tree delegate must sign.")]
+    TreeAuthorityIncorrect,
+    #[msg("Not enough unapproved mints left")]
+    InsufficientMintCapacity,
+    #[msg("NumericalOverflowError")]
+    NumericalOverflowError,
+    #[msg("Incorrect account owner")]
+    IncorrectOwner,
+    #[msg("Cannot Verify Collection in this Instruction")]
+    CollectionCannotBeVerifiedInThisInstruction,
+    #[msg("Collection Not Found on Metadata")]
+    CollectionNotFound,
+    #[msg("Collection item is already verified.")]
+    AlreadyVerified,
+    #[msg("Collection item is already unverified.")]
+    AlreadyUnverified,
+    #[msg("Incorrect leaf metadata update authority.")]
+    UpdateAuthorityIncorrect,
+    #[msg("This transaction must be signed by either the leaf owner or leaf delegate")]
+    LeafAuthorityMustSign,
+    #[msg("Collection Not Compatable with Compression, Must be Sized")]
+    CollectionMustBeSized,
 }
