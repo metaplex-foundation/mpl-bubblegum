4ba4fce15da2f6f1dba4055921b60c25e0a7c285
Loris Leiva <loris.leiva@gmail.com>
2023-07-26 14:03:20 +0100

    Add delegate and delegated transfer tests

---
+++ clients/js/test/delegate.test.ts
@@ -0,0 +1,50 @@
+import { generateSigner, publicKey } from '@metaplex-foundation/umi';
+import test from 'ava';
+import {
+  delegate,
+  fetchMerkleTree,
+  getCurrentRoot,
+  hashLeaf,
+  hashMetadataCreators,
+  hashMetadataData,
+} from '../src';
+import { createTree, createUmi, mint } from './_setup';
+
+test('it can delegate a compressed NFT', async (t) => {
+  // Given a tree with a minted NFT.
+  const umi = await createUmi();
+  const merkleTree = await createTree(umi);
+  let merkleTreeAccount = await fetchMerkleTree(umi, merkleTree);
+  const leafOwner = generateSigner(umi);
+  const { metadata, leafIndex } = await mint(umi, {
+    merkleTree,
+    leafOwner: leafOwner.publicKey,
+  });
+
+  // When the owner of the NFT delegates it to another account.
+  const newDelegate = generateSigner(umi).publicKey;
+  await delegate(umi, {
+    leafOwner,
+    previousLeafDelegate: leafOwner.publicKey,
+    newLeafDelegate: newDelegate,
+    merkleTree,
+    root: getCurrentRoot(merkleTreeAccount.tree),
+    dataHash: hashMetadataData(metadata),
+    creatorHash: hashMetadataCreators(metadata.creators),
+    nonce: leafIndex,
+    index: leafIndex,
+  })
+    .addRemainingAccounts([]) // <- Proof nodes would be added as remaining accounts.
+    .sendAndConfirm(umi);
+
+  // Then the leaf was updated in the merkle tree.
+  const updatedLeaf = hashLeaf(umi, {
+    merkleTree,
+    owner: leafOwner.publicKey,
+    delegate: newDelegate,
+    leafIndex,
+    metadata,
+  });
+  merkleTreeAccount = await fetchMerkleTree(umi, merkleTree);
+  t.is(merkleTreeAccount.tree.rightMostPath.leaf, publicKey(updatedLeaf));
+});
--- clients/js/test/transfer.test.ts
+++ clients/js/test/transfer.test.ts
@@ -1,6 +1,7 @@
 import { generateSigner, publicKey } from '@metaplex-foundation/umi';
 import test from 'ava';
 import {
+  delegate,
   fetchMerkleTree,
   getCurrentRoot,
   hashLeaf,
@@ -40,6 +41,57 @@ test('it can transfer a compressed NFT', async (t) => {
   const updatedLeaf = hashLeaf(umi, {
     merkleTree,
     owner: leafOwnerB.publicKey,
+    leafIndex,
+    metadata,
+  });
+  merkleTreeAccount = await fetchMerkleTree(umi, merkleTree);
+  t.is(merkleTreeAccount.tree.rightMostPath.leaf, publicKey(updatedLeaf));
+});
+
+test('it can transfer a compressed NFT as a delegated authority', async (t) => {
+  // Given a tree with a delegated compressed NFT owned by leafOwnerA.
+  const umi = await createUmi();
+  const merkleTree = await createTree(umi);
+  let merkleTreeAccount = await fetchMerkleTree(umi, merkleTree);
+  const leafOwnerA = generateSigner(umi);
+  const delegateAuthority = generateSigner(umi);
+  const { metadata, leafIndex } = await mint(umi, {
+    merkleTree,
+    leafOwner: leafOwnerA.publicKey,
+  });
+  await delegate(umi, {
+    leafOwner: leafOwnerA,
+    previousLeafDelegate: leafOwnerA.publicKey,
+    newLeafDelegate: delegateAuthority.publicKey,
+    merkleTree,
+    root: getCurrentRoot(merkleTreeAccount.tree),
+    dataHash: hashMetadataData(metadata),
+    creatorHash: hashMetadataCreators(metadata.creators),
+    nonce: leafIndex,
+    index: leafIndex,
+  }).sendAndConfirm(umi);
+
+  // When the delegated authority transfers the NFT to leafOwnerB.
+  const leafOwnerB = generateSigner(umi);
+  await transfer(umi, {
+    leafDelegate: delegateAuthority, // <- The delegated authority signs the transaction.
+    leafOwner: leafOwnerA.publicKey,
+    newLeafOwner: leafOwnerB.publicKey,
+    merkleTree,
+    root: getCurrentRoot(merkleTreeAccount.tree),
+    dataHash: hashMetadataData(metadata),
+    creatorHash: hashMetadataCreators(metadata.creators),
+    nonce: leafIndex,
+    index: leafIndex,
+  })
+    .addRemainingAccounts([]) // <- Proof nodes would be added as remaining accounts.
+    .sendAndConfirm(umi);
+
+  // Then the leaf was updated in the merkle tree.
+  const updatedLeaf = hashLeaf(umi, {
+    merkleTree,
+    owner: leafOwnerB.publicKey,
+    delegate: leafOwnerB.publicKey, // <- The delegated authority is removed.
     leafIndex,
     metadata,
   });
