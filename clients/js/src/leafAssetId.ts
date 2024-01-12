c31284dd0d268cab602db342774a073bd89831d8
blockiosaurus <blockiosaurus@gmail.com>
2024-01-12 14:13:18 -0500

    Adding ability to fetch Leaf Data from a TX.

--- clients/js/src/leafAssetId.ts
+++ clients/js/src/leafAssetId.ts
@@ -1,6 +1,6 @@
-import { Context, Pda, PublicKey } from '@metaplex-foundation/umi';
+import { Context, Pda, PublicKey, TransactionSignature } from '@metaplex-foundation/umi';
 import { publicKey, string, u64 } from '@metaplex-foundation/umi/serializers';
-import { MPL_BUBBLEGUM_PROGRAM_ID } from './generated';
+import { LeafSchema, MPL_BUBBLEGUM_PROGRAM_ID, getLeafSchemaSerializer } from './generated';
 
 export function findLeafAssetIdPda(
   context: Pick<Context, 'programs' | 'eddsa'>,
@@ -18,4 +18,34 @@ export function findLeafAssetIdPda(
     publicKey().serialize(seeds.merkleTree),
     u64().serialize(seeds.leafIndex),
   ]);
+}
+
+export async function parseLeafFromMintV1Transaction(
+  context: Pick<Context, 'programs' | 'eddsa' | 'rpc'>,
+  signature: TransactionSignature
+): Promise<LeafSchema> {
+  const transaction = await context.rpc.getTransaction(signature);
+  const innerInstructions = transaction?.meta.innerInstructions;
+
+  if (innerInstructions) {
+    const leaf = getLeafSchemaSerializer().deserialize(innerInstructions[0].instructions[0].data.slice(8));
+    return leaf[0];
+  }
+
+  throw new Error('Could not parse leaf from transaction');
+}
+
+export async function parseLeafFromMintToCollectionV1Transaction(
+  context: Pick<Context, 'programs' | 'eddsa' | 'rpc'>,
+  signature: TransactionSignature
+): Promise<LeafSchema> {
+  const transaction = await context.rpc.getTransaction(signature);
+  const innerInstructions = transaction?.meta.innerInstructions;
+
+  if (innerInstructions) {
+    const leaf = getLeafSchemaSerializer().deserialize(innerInstructions[0].instructions[1].data.slice(8));
+    return leaf[0];
+  }
+
+  throw new Error('Could not parse leaf from transaction');
 }
