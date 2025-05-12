efcb53f275fbbfefa58af4cd8e73ff6704b343dc
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2025-05-12 14:09:50 -0700

    One more mint test

--- clients/js/test/mintV2_only.test.ts
+++ clients/js/test/mintV2_only.test.ts
@@ -3,11 +3,17 @@ import {
   generateSigner,
   publicKey,
   sol,
+  none,
 } from '@metaplex-foundation/umi';
 import test from 'ava';
 
 import { fetchMerkleTree } from '@metaplex-foundation/mpl-account-compression';
-import { AssetDataSchema, hashLeafV2 } from '../src';
+import {
+  AssetDataSchema,
+  hashLeafV2,
+  MetadataArgsV2Args,
+  mintV2 as baseMintV2,
+} from '../src';
 import { createTreeV2, createUmi, mintV2 } from './_setup';
 
 test('it can mint a compressed NFT using V2 instructions', async (t) => {
@@ -74,6 +80,38 @@ test('it cannot mint a compressed NFT with asset data using V2 instructions', as
   });
 
   await t.throwsAsync(promise, { name: 'NotAvailable' });
+
+  // Then the rightmost leaf is still the default `Publickey`.
+  const merkleTreeAccount = await fetchMerkleTree(umi, merkleTree);
+  t.is(merkleTreeAccount.tree.sequenceNumber, 0n);
+  t.is(
+    merkleTreeAccount.tree.rightMostPath.leaf,
+    publicKey(defaultPublicKey())
+  );
+});
+
+test('it cannot mint a compressed NFT with too long name', async (t) => {
+  // Given a tree with a minted NFT owned by leafOwnerA.
+  const umi = await createUmi();
+  const merkleTree = await createTreeV2(umi);
+  const leafOwner = generateSigner(umi);
+
+  // When we mint a new NFT from the tree using the following metadata.
+  const metadata: MetadataArgsV2Args = {
+    name: 'My NFT with a name that is over 32 characters',
+    uri: 'https://example.com/my-nft.json',
+    sellerFeeBasisPoints: 500, // 5%
+    collection: none(),
+    creators: [],
+  };
+  const promise = baseMintV2(umi, {
+    leafOwner: leafOwner.publicKey,
+    merkleTree,
+    metadata,
+  }).sendAndConfirm(umi);
+
+  // Then we expect an error.
+  await t.throwsAsync(promise, { name: 'MetadataNameTooLong' });
 
   // Then the rightmost leaf is still the default `Publickey`.
   const merkleTreeAccount = await fetchMerkleTree(umi, merkleTree);
